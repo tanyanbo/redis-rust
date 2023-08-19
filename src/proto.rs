ad03e0a77b8115f47cab74540dbbbfe90f9633a8
Tan Yanbo <tanyanbo95@hotmail.com>
2023-08-19 08:23:15 +0800

    wip protocol parser

--- src/proto.rs
+++ src/proto.rs
@@ -10,54 +10,49 @@ pub enum Command {
     Array { values: Vec<String> },
 }
 
-pub struct Parser;
+pub fn parse<'a>(command: &'a [u8]) -> Result<Command, ParserError> {
+    parse_internal(command)
+}
 
-impl Parser {
-    pub fn parse<'a>(command: &'a [u8]) -> Result<Command, ParserError> {
-        Self::parse_internal(command)
+
+fn parse_internal<'a>(command: &'a [u8]) -> Result<Command, ParserError> {
+    let message = String::from_utf8_lossy(command).to_lowercase();
+    let first_byte = command.get(0).ok_or(ParserError::EmptyCommand)?;
+    let message = &message.trim().get(1..).ok_or(ParserError::OnlyOneByte)?;
+    let commands = message.split("\r\n");
+
+    match first_byte {
+        b'*' => parse_array(commands),
+        b'+' => parse_simple_string(commands),
+        _ => unimplemented!("error"),
+    }
+}
+
+fn parse_array<'a>(mut commands: impl Iterator<Item = &'a str>) -> Result<Command, ParserError> {
+    let mut elements_len: usize = match commands.next() {
+        Some(x) => x
+            .parse()
+            .map_err(|_| ParserError::InvalidLength(x.to_string()))?,
+        _ => return Err(ParserError::OnlyOneByte),
+    };
+
+    let mut values = vec![];
+    while let Some(command) = commands.next() {
+        elements_len -= 1;
+        values.push(command.to_string());
     }
 
-    fn parse_internal<'a>(command: &'a [u8]) -> Result<Command, ParserError> {
-        let message = String::from_utf8_lossy(command).to_lowercase();
-        let first_byte = command.get(0).ok_or(ParserError::EmptyCommand)?;
-        let message = &message.trim().get(1..).ok_or(ParserError::OnlyOneByte)?;
-        let commands = message.split("\r\n");
-
-        match first_byte {
-            b'*' => Self::parse_array(commands),
-            b'+' => Self::parse_simple_string(commands),
-            _ => unimplemented!("error"),
-        }
+    if elements_len != 0 {
+        return Err(ParserError::WrongLength(elements_len));
     }
 
-    fn parse_array<'a>(
-        mut commands: impl Iterator<Item = &'a str>,
-    ) -> Result<Command, ParserError> {
-        let mut elements_len: usize = match commands.next() {
-            Some(x) => x
-                .parse()
-                .map_err(|_| ParserError::InvalidLength(x.to_string()))?,
-            _ => return Err(ParserError::OnlyOneByte),
-        };
+    Ok(Command::Array { values })
+}
 
-        let mut values = vec![];
-        while let Some(command) = commands.next() {
-            elements_len -= 1;
-            values.push(command.to_string());
-        }
-
-        if elements_len != 0 {
-            return Err(ParserError::WrongLength(elements_len));
-        }
-
-        Ok(Command::Array { values })
-    }
-
-    fn parse_simple_string<'a>(
-        mut commands: impl Iterator<Item = &'a str>,
-    ) -> Result<Command, ParserError> {
-        Ok(Command::SimpleString {
-            value: commands.next().ok_or(ParserError::OnlyOneByte)?.to_string(),
-        })
-    }
+fn parse_simple_string<'a>(
+    mut commands: impl Iterator<Item = &'a str>,
+) -> Result<Command, ParserError> {
+    Ok(Command::SimpleString {
+        value: commands.next().ok_or(ParserError::OnlyOneByte)?.to_string(),
+    })
 }
